36cad4e8b93902861d6239b211d3bfca77f7e298
Vitali Khileuski <kentkentkent28@gmail.com>
2021-02-25 10:25:51 +0300

    create new project

--- TwitterTrends/TwitterTrends/MainWindow.xaml.cs
+++ TwitterTrends/TwitterTrends/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace TwitterTrends
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
