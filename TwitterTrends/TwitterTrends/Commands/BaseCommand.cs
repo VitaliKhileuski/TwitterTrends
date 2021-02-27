23e3a9fc548616d68240d3e07a9cd5b1a9efd5b7
Vitali Khileuski <kentkentkent28@gmail.com>
2021-02-27 21:31:56 +0300

    Base classes for MVVM

--- TwitterTrends/TwitterTrends/Commands/BaseCommand.cs
+++ TwitterTrends/TwitterTrends/Commands/BaseCommand.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace TwitterTrends.Commands
 {
-    class BaseCommand
+   internal abstract class BaseCommand : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
     }
 }
--- TwitterTrends/TwitterTrends/Commands/Command.cs
+++ TwitterTrends/TwitterTrends/Commands/Command.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace TwitterTrends.Commands
 {
-    class Command
+    internal class Command : BaseCommand
     {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        
+        
+        public Command(Action<object> Execute, Func<object,bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+
     }
 }
