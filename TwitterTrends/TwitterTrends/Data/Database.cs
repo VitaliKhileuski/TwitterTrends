c465504e0aed53306b26ba9ca2b53ca492f0cd4f
DmitryMashkov <55850305+B1iz0@users.noreply.github.com>
2021-03-03 10:48:19 +0300

    Create Database.cs

---
+++ TwitterTrends/TwitterTrends/Data/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitterTrends.Models;
+using TwitterTrends.Models.Parsers;
+
+namespace TwitterTrends.Data
+{
+    class Database
+    {
+        private static Database Instance = null;
+
+        private List<Tweet> tweets = TweetParser.Parse(@"C:\Users\mashk\source\repos\TwitterTrends\TwitterTrends\TwitterTrends\Data\Tweets\cali_tweets2014.txt");
+
+        public List<Tweet> Tweets { get { return tweets; } }
+
+        private Database()
+        {
+
+        }
+
+        public static Database GetInstance()
+        {
+            if(Instance == null)
+            {
+                Instance = new Database();
+            }
+            return Instance;
+        }
+    }
+}
