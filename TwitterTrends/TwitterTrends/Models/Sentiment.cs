129adf47a77f0bc9689f116a3c6971d20251e2e9
Vitali Khileuski <kentkentkent28@gmail.com>
2021-03-09 08:42:34 +0300

    create sentiment class for our dictionary

---
+++ TwitterTrends/TwitterTrends/Models/Sentiment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterTrends.Models
+{
+    public class Sentiment
+    {
+        private string text;
+        private int numberOfWords;
+        private double value;
+
+        public string Text { get { return text; } set { text = value; } }
+        public int NumberOfWords { get { return numberOfWords; } set { numberOfWords = value; } }
+        private double Value { get { return value;  } set { this.value = value; } }
+
+        public Sentiment()
+        {
+
+        }
+        public Sentiment(string text, double value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+}
