a5272cedad08814503619c0cd433a8f4ec6240ee
ales122 <74967961+ales122@users.noreply.github.com>
2021-03-18 08:54:29 +0300

    edit map

--- TwitterTrends/TwitterTrends/MainWindow.xaml.cs
+++ TwitterTrends/TwitterTrends/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TwitterTrends.Models;
+using TwitterTrends.Models.Parsers;
 using TwitterTrends.Services.Parsers;
 using TwitterTrends.ViewModels;
 using Brushes = System.Windows.Media.Brushes;
@@ -51,18 +52,19 @@ namespace TwitterTrends
             gmap.CanDragMap = true;
             gmap.DragButton = MouseButton.Left;
 
+
             gmap.MaxZoom = 18;
             gmap.MinZoom = 3;
 
-            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;  
+            gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
 
             gmap.ShowTileGridLines = false;
-            gmap.Zoom = 4;
+            gmap.Zoom = 2;
             gmap.ShowCenter = false;
 
             gmap.MapProvider = GMapProviders.BingMap;
             GMaps.Instance.Mode = AccessMode.ServerOnly;
-            gmap.Position = new PointLatLng(38.8951100, -77.0363700);
+            gmap.Position = new PointLatLng(51.39920565355378, -108.63281250000001);
 
 
             GMapProvider.WebProxy = WebRequest.GetSystemWebProxy();
@@ -70,60 +72,58 @@ namespace TwitterTrends
 
             Country country;
             country = StatesParser.Parse("E:\\states.json");
-            List<GMapPolygon> polygons;
 
             foreach (var state in country.States)
             {
-                List<PointLatLng> pointlatlang1= new List<PointLatLng>();
-                foreach (var polygon1 in state.Polygons)
-                {
-                    polygons = new List<GMapPolygon>();
 
-                    foreach (var point in polygon1.Points)
+                foreach (var polygon in state.Polygons)
+                {
+                    List<PointLatLng> pointlatlang = new List<PointLatLng>();
+                    GMapPolygon pol = new GMapPolygon(pointlatlang);
+                    foreach (var point in polygon.Points)
                     {
-                        pointlatlang1.Add(new PointLatLng(point.Y, point.X));
+                        pointlatlang.Add(new PointLatLng(point.Y, point.X));
                     }
-                    GMapPolygon pol = new GMapPolygon(pointlatlang1);
+                    pol.Points = pointlatlang;
                     gmap.RegenerateShape(pol);
-                    (pol.Shape as Path).Fill = Brushes.Red;
+                    (pol.Shape as Path).Fill = Brushes.Blue;
                     (pol.Shape as Path).Stroke = Brushes.Blue;
                     (pol.Shape as Path).StrokeThickness = 1.5;
                     (pol.Shape as Path).Effect = null;
                     gmap.Markers.Add(pol);
                 }
-              
+
             }
 
 
 
+            //          List<PointLatLng> pointlatlang= new List<PointLatLng>()
+            //          {
 
-  //          List<PointLatLng> pointlatlang= new List<PointLatLng>()
-  //          {
+            //new PointLatLng( 39.804456,-75.414089),
+            //          new PointLatLng(39.683964,-75.507197),
+            //           new PointLatLng(39.61824,-75.611259),
+            //            new PointLatLng(39.459409,-75.589352),
+            //             new PointLatLng(39.311532,-75.441474),
+            //               new PointLatLng(39.065069,-75.403136),
+            //          new PointLatLng(38.807653,-75.189535),
+            //           new PointLatLng(38.796699,-75.09095),
+            //            new PointLatLng(38.451652,-75.047134),
+            //             new PointLatLng(38.462606,-75.693413),
+            //               new PointLatLng(39.722302,-75.786521),
+            //          new PointLatLng(39.831841,-75.616736),
+            //             new PointLatLng(39.804456,-75.414089)
+            //      };
 
-  //new PointLatLng( 39.804456,-75.414089),
-  //          new PointLatLng(39.683964,-75.507197),
-  //           new PointLatLng(39.61824,-75.611259),
-  //            new PointLatLng(39.459409,-75.589352),
-  //             new PointLatLng(39.311532,-75.441474),
-  //               new PointLatLng(39.065069,-75.403136),
-  //          new PointLatLng(38.807653,-75.189535),
-  //           new PointLatLng(38.796699,-75.09095),
-  //            new PointLatLng(38.451652,-75.047134),
-  //             new PointLatLng(38.462606,-75.693413),
-  //               new PointLatLng(39.722302,-75.786521),
-  //          new PointLatLng(39.831841,-75.616736),
-  //             new PointLatLng(39.804456,-75.414089)
-  //      };
+            //          GMapPolygon polygon = new GMapPolygon(pointlatlang);
+            //          gmap.RegenerateShape(polygon);
+            //          (polygon.Shape as Path).Fill = Brushes.Red;
+            //         (polygon.Shape as Path).Stroke =Brushes.Blue;
+            //          (polygon.Shape as Path).StrokeThickness = 1.5;
+            //          (polygon.Shape as Path).Effect = null;
+            //          gmap.Markers.Add(polygon);
 
-  //          GMapPolygon polygon = new GMapPolygon(pointlatlang);
-  //          gmap.RegenerateShape(polygon);
-  //          (polygon.Shape as Path).Fill = Brushes.Red;
-  //         (polygon.Shape as Path).Stroke =Brushes.Blue;
-  //          (polygon.Shape as Path).StrokeThickness = 1.5;
-  //          (polygon.Shape as Path).Effect = null;
-  //          gmap.Markers.Add(polygon);
-
-
+           
 
         }
     }
